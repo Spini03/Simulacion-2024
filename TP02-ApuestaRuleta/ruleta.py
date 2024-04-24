02faf472599fd0c2fd7bce414750006ec22bdb47
santipdmonte <santiagopedemonte02@gmail.com>
2024-04-24 10:14:03 -0300

    nueva rula

--- TP02-ApuestaRuleta/ruleta.py
+++ TP02-ApuestaRuleta/ruleta.py
@@ -1,32 +1,24 @@
-import random
+# Para la gestion de argumentos de linea de comandos
+import argparse 
 import sys
+
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 import statistics as st
-import argparse  # Para la gestion de argumentos de linea de comandos
-
-rul = np.arange(37)
-aciertos = 0
-prom_esperado = np.mean(rul)
-valor_fr_esperada = 1 / 37
-valor_var_esperada = np.var(rul)
-valor_desvio_esperado = np.std(rul)
-todos_los_valores = []
-todas_las_tiradas = []
-valores = [1000]
-prom = []
-frecrel = []
-var = []
-des = []
 
 
-def definir_valores():
+# Nuevas Variables
+apuesta_minima = 1
+apuesta_par = True
+
+def inicializar_valores():
     parser = argparse.ArgumentParser(description='Script para procesar argumentos de línea de comandos')
     parser.add_argument('-c', '--cant_tiradas', type=int, required=True, help='Cantidad de tiradas')
     parser.add_argument('-n', '--corridas', type=int, required=True, help='Número de corridas')
-    parser.add_argument('-e', '--num_elegido', type=int, required=False, help='Número elegido')
     parser.add_argument('-s', '--estrategia', type=str, choices=['m', 'd', 'f', 'o'], required=True, help='Opciones permitidas: m, d, f, o')
     parser.add_argument('-a', '--tipo_capital', type=str, choices=['i', 'f'], required=True, help='Opciones permitidas: m, d, f, o')
+    parser.add_argument('-e', '--num_elegido', type=int, required=False, help='Número elegido')
 
     args = parser.parse_args()
 
@@ -41,68 +33,118 @@ def definir_valores():
     return args.cant_tiradas, args.corridas, args.num_elegido, args.estrategia, args.tipo_capital
 
 
-cant_tiradas, corridas, num_elegido, estrategia, tipo_capital = definir_valores()
+def generar_listas_esperadas(cant_tiradas):
 
-# listas valores esperados
-list_prom_esperado = []
-list_fr_esperada = []
-list_var_esperada = []
-list_des_esperado = []
-for l in range(cant_tiradas):
-    list_prom_esperado.append(prom_esperado)
-    list_fr_esperada.append(valor_fr_esperada)
-    list_var_esperada.append(valor_var_esperada)
-    list_des_esperado.append(valor_desvio_esperado)
+    ruleta = np.arange(37)
+
+    prom_esperado = np.mean(ruleta)
+    fr_esperada = 1 / 37
+    var_esperada = np.var(ruleta)
+    des_esperado = np.std(ruleta)
+
+    list_prom_esperado = [prom_esperado] * cant_tiradas
+    list_fr_esperada = [fr_esperada] * cant_tiradas
+    list_var_esperada = [var_esperada] * cant_tiradas
+    list_des_esperado = [des_esperado] * cant_tiradas
+
+    return list_prom_esperado, list_fr_esperada, list_var_esperada, list_des_esperado
 
 
-def graficar(i, prom, frecrel, var, des):
-    fig, axs = plt.subplots(nrows=2, ncols=2)
+def graficar(promedio, frec_rel, varianza, desvio, cant_tiradas, valores, num_corrida):
+    fig, axs = plt.subplots(nrows=3, ncols=2)
     axs = axs.flatten()
 
-    titulo = 'Datos corrida ' + str(i + 1)
+    list_prom_esperado, list_frec_esperada, list_var_esperada, list_des_esperado = generar_listas_esperadas(cant_tiradas)
+
+    titulo = 'Datos corrida ' + str(num_corrida)
     fig.suptitle(titulo)
-    axs[0].plot(prom)
-    axs[0].plot(list_prom_esperado)
+
+    # Frec Relativa
+    axs[0].set_title('Frecuencia Relativa del numero 14')
+    axs[0].set_ylabel('Frecuencia relativa')
+    axs[0].plot(frec_rel)
+    axs[0].plot(list_frec_esperada)
     axs[0].set_xlabel('Número de tirada')
-    axs[0].set_ylabel('Promedio')
-    axs[1].plot(frecrel)
-    axs[1].plot(list_fr_esperada)
+
+    # Promedio
+    axs[1].set_title('Promedio')
+    axs[1].set_ylabel('Promedio')
+    axs[1].plot(promedio)
+    axs[1].plot(list_prom_esperado)
     axs[1].set_xlabel('Número de tirada')
-    axs[1].set_ylabel('Frecuencia relativa')
-    axs[2].plot(var)
-    axs[2].plot(list_var_esperada)
+
+    # Desvio
+    axs[2].set_title('Desvíos')
+    axs[2].set_ylabel('Desvio')
+    axs[2].plot(desvio)
+    axs[2].plot(list_des_esperado)
     axs[2].set_xlabel('Número de tirada')
-    axs[2].set_ylabel('Varianza')
-    axs[3].plot(des)
-    axs[3].plot(list_des_esperado)
+
+    # Varianza
+    axs[3].set_title('Varianza')
+    axs[3].set_ylabel('Varianza')
+    axs[3].plot(varianza)
+    axs[3].plot(list_var_esperada)
     axs[3].set_xlabel('Número de tirada')
-    axs[3].set_ylabel('Desvio')
+
+    # Frecuencia Acumulada
+    axs[4].set_title('Frecuencia Acumulada')
+    axs[4].bar(range(37), np.histogram(valores, bins=range(38))[0])
+    axs[4].set_ylabel('Frecuencia')
+    axs[4].set_xlabel('Número')
+
+    # Frecuecnia Relativa
+    axs[5].set_title('Frecuencia Relativa')
+    axs[5].hist(valores, bins=range(38), density=True, alpha=0.75)
+    axs[5].set_ylabel('Frecuencia')
+    axs[5].set_xlabel('Número')
+
     fig.tight_layout()
     plt.show()
 
 
-for i in range(corridas):
-    valores.clear()
-    prom.clear()
-    frecrel.clear()
-    var.clear()
-    des.clear()
-    ganadas = 0
-    for j in range(cant_tiradas):
-        num = random.randint(0, 36)
-        valores.append(num)
-        prom.append(np.mean(valores))
-        if num_elegido == num:
-            ganadas += 1
-        frecrel.append(ganadas / (j + 1))
-        if j > 0:
-            var.append(st.variance(valores))
-            des.append(st.stdev(valores))
-    todos_los_valores.append(valores)
-    print(valores)
-    print("Aciertos corrida: ", ganadas)
-    aciertos = aciertos + ganadas
+def ejecutar_corridas(cant_tiradas, cant_corridas, num_elegido):
+    aciertos = 0
 
-    graficar(i, prom, frecrel, var, des)
+    for i in range(cant_corridas):
 
-print("Aciertos totales: ", aciertos)
+        print(f"\n======== CORRIDA {i + 1} ========")
+        
+        valores = []
+        promedio = []
+        frecrel = []
+        var = []
+        des = []
+        ganadas = 0
+
+        for j in range(cant_tiradas):
+
+            num = random.randint(0, 36)
+            valores.append(num)
+            promedio.append(np.mean(valores))
+
+            ganadas += 1 if num_elegido == num else 0
+
+            frecrel.append(ganadas / (j + 1))
+
+            if j > 0: 
+                var.append(st.variance(valores))
+                des.append(st.stdev(valores))
+
+        print("Valores Ruleta: ", valores)
+        print("Aciertos corrida: ", ganadas)
+
+        aciertos = aciertos + ganadas
+
+        graficar(promedio, frecrel, var, des, cant_tiradas, valores, i + 1)
+
+    print(f"\n======== FIN CORRIDAS ========\n")
+    print("Aciertos totales: ", aciertos)
+
+
+def main():
+    cant_tiradas, cant_corridas, num_elegido, estrategia, capital = inicializar_valores()
+    ejecutar_corridas(cant_tiradas, cant_corridas, num_elegido)
+
+if __name__ == '__main__':
+    main()
