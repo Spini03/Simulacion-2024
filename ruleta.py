676df1ec0e6754750618face23bd58ec7e6468b0
Eneasss <eneasparatore@gmail.com>
2024-04-29 17:30:34 -0300

    cambios

--- ruleta.py
+++ ruleta.py
@@ -10,19 +10,26 @@ if len(sys.argv) != 7 or sys.argv[1] != "-c" or sys.argv[3] != "-n" or sys.argv[
 cant_tiradas = int(sys.argv[2])
 corridas = int(sys.argv[4])
 num_elegido = int(sys.argv[6])
+valores = list()
+y1 = list()
 #valores = [random.randint(0, 36) for _ in range(cant_tiradas)]
 for i in range(corridas):
-        valores = [random.randint(0, 36) for j in range(cant_tiradas)]
-        print(valores)
-        aciertos_corrida = valores.count(num_elegido)
-        print("Aciertos corrida: ", aciertos_corrida)
-        aciertos = aciertos + aciertos_corrida
+    valores[0] = random.randint(0, 36)
+    y1[0] = valores[0] / 1
+    for j in range(1,cant_tiradas):
+        valores[j] = random.randint(0, 36)
+        y1[j] = ( y1[j-1] * j-1 + valores[j] ) / j
+    print(valores)
+    aciertos_corrida = valores.count(num_elegido)
+    print("Aciertos corrida: ", aciertos_corrida)
+    aciertos = aciertos + aciertos_corrida
 
 print(aciertos)
 
 total_tiradas = corridas*cant_tiradas
 
 x1 = list(range(cant_tiradas))
+
 
 
 
