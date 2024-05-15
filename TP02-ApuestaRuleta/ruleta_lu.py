73efc7c23c8e5b95c0e9ca620a0999d04457f7b7
santipdmonte <santiagopedemonte02@gmail.com>
2024-05-15 09:40:35 -0300

    Nuevo Saldo Inicial 100.000

--- TP02-ApuestaRuleta/ruleta_lu.py
+++ TP02-ApuestaRuleta/ruleta_lu.py
@@ -308,7 +308,7 @@ def nueva_estrategia(es_ganador, apuesta_inicial, apuesta_anterior):
 
 
 def main():
-    saldo = 10000  # TODO: Justificar el minimo ingresado
+    saldo = 100000  # TODO: Justificar el minimo ingresado
     #Suponiendo que son 100000 pesos argentinos. Como son 10000 tiradas me pareció que tiene sentido.
     apuesta_inicial = 10
     apuesta_par = True
