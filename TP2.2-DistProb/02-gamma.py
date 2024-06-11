51faf55c77a98af2c2d560b5ce30765f91843fc2
Spini03 <santiagospini@gmail.com>
2024-06-11 16:04:37 -0300

    mismo formato para las gráficas

--- TP2.2-DistProb/02-gamma.py
+++ TP2.2-DistProb/02-gamma.py
@@ -17,13 +17,19 @@ uniform_samples = np.random.rand(1000)
 gamma_samples = transform_uniform_to_gamma(uniform_samples, k, theta)
 
 # Graficamos los resultados
-plt.hist(gamma_samples, bins=30, density=True, alpha=0.6, color='g')
+plt.hist(gamma_samples, bins=30, density=True, alpha=0.6, color='g', label='Datos simulados')
 
 # Graficamos la función teórica
 xmin, xmax = plt.xlim()
 x = np.linspace(xmin, xmax, 100)
 p = gamma.pdf(x, a=k, scale=theta)
-plt.plot(x, p, 'k', linewidth=2)
-title = "Histograma de datos generados y la función teórica de la Gamma"
-plt.title(title)
+plt.plot(x, p, 'k', linewidth=2, label='Función teórica')
+
+# Configuración de la gráfica
+plt.xlabel('Valor')
+plt.ylabel('Densidad de probabilidad')
+plt.title('Distribución Gamma')
+plt.legend()
+
+# Mostrar la gráfica
 plt.show()
--- TP2.2-DistProb/04-uniforme.py
+++ TP2.2-DistProb/04-uniforme.py
@@ -12,13 +12,19 @@ b = 10
 valores_uniforme = generar_uniforme(a, b, 1000000)
 
 # Graficar los resultados
-plt.hist(valores_uniforme, bins=30, density=True, alpha=0.6, color='g')
+plt.hist(valores_uniforme, bins=30, density=True, alpha=0.6, color='g', label='Datos simulados')
 
 # Graficar la función teórica
 xmin, xmax = plt.xlim()
 x = np.linspace(xmin, xmax, 100)
 p = [1 / (b - a) if a <= val <= b else 0 for val in x]
-plt.plot(x, p, 'k', linewidth=2)
-title = "Histograma de datos generados y la función teórica de la Uniforme"
-plt.title(title)
+plt.plot(x, p, 'k', linewidth=2, label='Función teórica')
+
+# Configuración de la gráfica
+plt.xlabel('Valor')
+plt.ylabel('Densidad de probabilidad')
+plt.title('Distribución Uniforme')
+plt.legend()
+
+# Mostrar la gráfica
 plt.show()
